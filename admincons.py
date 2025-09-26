06143be412dca517d8c60a5b4a20ebaec585688c
Deaadd <141892345+Deaadd@users.noreply.github.com>
2025-09-27 01:05:55 +0300

    fix admin coms

--- admincons.py
+++ admincons.py
@@ -18,31 +18,12 @@ def setup_admin(bot):
         if cur_id in information_data.admins_id:
             keyboard = types.InlineKeyboardMarkup()
             keyboard.add(types.InlineKeyboardButton(
-                text="🔄 Перезагрузить бота (автообновление с git)",
-                callback_data="ask_restart_bot"))
-            keyboard.add(types.InlineKeyboardButton(
-                text="❌ Остановить работу бота (крайний случай)",
+                text="❌ Остановить работу бота (обновление/рестарт)\n\nХост сам его перезапускает",
                 callback_data="ask_stop_bot"))
 
             bot.send_message(message.chat.id, 'Выберете нужное действие:', reply_markup=keyboard)
         else:
             bot.send_message(message.chat.id, '❌ У вас нет прав для выполнения данной команды!')
-
-    # Обработчик подтверждения перезапуска
-    @bot.callback_query_handler(func=lambda call: call.data == 'ask_restart_bot')
-    def ask_restart_bot(call):
-        keyboard = types.InlineKeyboardMarkup()
-        keyboard.row(
-            types.InlineKeyboardButton(text="✅ Подтвердить", callback_data="confirm_restart_bot"),
-            types.InlineKeyboardButton(text="❌ Отменить", callback_data="cancel_action")
-        )
-        bot.edit_message_text(
-            "⚠️ Вы уверены, что хотите перезагрузить бота?\n\n"
-            "Бот будет недоступен на время перезапуска.",
-            call.message.chat.id,
-            call.message.message_id,
-            reply_markup=keyboard
-        )
 
     # Обработчик подтверждения остановки
     @bot.callback_query_handler(func=lambda call: call.data == 'ask_stop_bot')
@@ -60,17 +41,6 @@ def setup_admin(bot):
             reply_markup=keyboard
         )
 
-    # Подтвержденный рестарт
-    @bot.callback_query_handler(func=lambda call: call.data == 'confirm_restart_bot')
-    def confirm_restart_bot(call):
-        bot.answer_callback_query(call.id, "🔄 Перезапуск...")
-        bot.edit_message_text(
-            "🔄 Перезапускаю бота...\n\n"
-            "Бот будет снова доступен через несколько секунд.",
-            call.message.chat.id,
-            call.message.message_id
-        )
-        threading.Timer(0, restart_bot_delayed).start()
 
     # Подтвержденная остановка
     @bot.callback_query_handler(func=lambda call: call.data == 'confirm_stop_bot')
@@ -104,17 +74,6 @@ def setup_admin(bot):
             reply_markup=keyboard
         )
 
-
-def restart_bot_delayed():
-    """Перезапуск процесса бота"""
-    time.sleep(2)
-    # try:
-    #     print("🔄 Обновление кода из git...")
-    #     # subprocess.call(["git", "pull"])
-    # except Exception as e:
-    #     print(f"⚠️ Ошибка git pull: {e}")
-    # print("♻️ Перезапуск Python процесса...")
-    os.execl(sys.executable, sys.executable, *sys.argv)
 
 
 def stop_bot_delayed():
